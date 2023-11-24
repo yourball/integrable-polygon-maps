275e82627a828cf5bf905716fc8f2334337f0547
yourball <y.kharkov@gmail.com>
2023-11-24 11:54:04 -0500

    fix thorus+periodic

--- web_polygons_plotly.py
+++ web_polygons_plotly.py
@@ -8,6 +8,7 @@ import time
 import matplotlib.pyplot as plt
 
 def force(x, k_list, xi_list, d):
+    # assert len(k_list) == len(xi_list) + 1
     # precompute parameters of the force function
     ai_list, bi_list = [], []
     for indx in range(len(k_list)):
@@ -29,16 +30,17 @@ def force(x, k_list, xi_list, d):
     f = ki * (x - ai) + bi + d
     return f
 
-def force_thorus(x, k_list, xi_list, d, L=1):
+def force_thorus(x, k_list, xi_list, d):
+    L = xi_list[-1]
     return force(x % L, k_list, xi_list, d) % L
 
 def periodic_step(x, width=1):
     return x // width
 
 def force_periodic(x, k_list, xi_list, d):
-    l = xi_list[-1]
-    delta_f = force(l, k_list, xi_list, d) - force(0, k_list, xi_list, d)
-    return delta_f*periodic_step(x, width=l) + force(x % l, k_list, xi_list, d)
+    L = xi_list[-1]
+    delta_f = force(L, k_list, xi_list, d) - force(0, k_list, xi_list, d)
+    return delta_f*periodic_step(x, width=L) + force(x % L, k_list, xi_list, d)
 
 def orbit(
     x0, y0, k_list, xi_list, d, Tmax, map_type="Simple", L=1
@@ -50,8 +52,9 @@ def orbit(
         if map_type == "Simple":
             x, y = y, -x + force(y, k_list, xi_list, d)
         elif map_type == "Thorus":
-            L = xi_list[-1]
-            x, y = y, -x + force_thorus(y , k_list, xi_list, d, L)
+            x, y = y, -x + force_thorus(y , k_list, xi_list, d)
+        elif map_type == "Periodic":
+            x, y = y, -x + force_periodic(y , k_list, xi_list, d)
 
         traj_list += [[x, y]]
     traj_data = np.vstack(traj_list)
@@ -103,21 +106,24 @@ with st.sidebar:
 
     with st.expander("Slopes of the piesewise function"):
         for p in range(num_pieces):
-            k_list.append(st.slider(f'Slope, k{p}', min_value=-3, max_value=3, value=0))
-    # with st.expander("Segment lengths of the piesewise function"):
-    #     for p in range(num_pieces):
-    #         l_list.append(st.slider(f'Segment length, l{p}', min_value=1, max_value=3, value=1))
+            k_list.append(st.slider(f'Slope, k{p}', min_value=-3., max_value=3., value=0., step=0.5))
+    with st.expander("Segment lengths of the piesewise function"):
+        num_lengths = num_pieces-2 if map_type == "Simple" else num_pieces
 
-    d = st.slider('Shift parameter, d', min_value=-10, max_value=10, value=0)
-    if (map_type == "Simple") or (map_type == "Thorus"):
-        xi_list = np.arange(len(k_list)-1)
+        for p in range(num_lengths):
+            l_list.append(st.slider(f'Segment length, l{p}', min_value=1., max_value=3., value=1., step=0.5))
+
+    d = st.slider('Shift parameter, d', min_value=-10., max_value=10., value=0., step=0.5)
+
+    if map_type == "Simple":
+        print("!!!", num_lengths)
+        # xi_list = np.arange(len(k_list)-1)
+        xi_list = np.array([sum(l_list[:i]) for i in range(len(k_list)-1)])
     else:
-        xi_list = np.arange(1, len(k_list)+1)
+        xi_list = np.array([sum(l_list[:i]) for i in range(len(k_list)+1)])
+
 
     Tmax = st.number_input("Number of map iterations", value=1000, min_value=1, step=100)
-
-    # if map_type == "Thorus":
-    #     L = st.slider('Period, L (thorus map)', min_value=1, max_value=10, value=2, step=1)
 
     st.divider()
     md = st.markdown(''':gray[@ Created by Yaroslav Kharkov, Tymofey Zolkin, Sergey Nagaitsev (2023)]''')
@@ -128,6 +134,15 @@ with tab1:
     st.subheader('Mapping')
 
     print("Plot orbits")
+    print("k_list", k_list)
+    if (map_type == "Periodic") or (map_type=="Thorus"):
+        k_list = [k_list[0]] + k_list + [k_list[-1]]
+
+    print("k_list", k_list)
+    print("l_list", l_list)
+    print("xi_list", xi_list)
+    print(map_type)
+
     fig_map = plot_orbits(k_list, xi_list, d=d, Tmax=Tmax, map_type=map_type)
     st.plotly_chart(fig_map)
 
@@ -136,13 +151,12 @@ with tab1:
     x = np.linspace(min(xi_list)-5, max(xi_list)+5, 1000)
     f = []
 
-
-    print(k_list, xi_list)
+    print(k_list, xi_list, l_list)
     for xi in x:
         if map_type == "Simple":
             f.append(force(xi, k_list, xi_list, d))
         elif map_type == "Thorus":
-            f.append(force_thorus(xi, k_list, xi_list, d, L=xi_list[-1]))
+            f.append(force_thorus(xi, k_list, xi_list, d))
         elif map_type == "Periodic":
             f.append(force_periodic(xi, k_list, xi_list, d))
 
@@ -153,6 +167,7 @@ with tab1:
         height=600,
       )
     st.plotly_chart(fig_f)
+    plt.legend()
 with tab2:
     st.text("Symplectic (Hamiltonian) map in McMillan-Turaev form:")
     st.latex(r'''
