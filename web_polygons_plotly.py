20ba854f822f7b21ae5a534338e76129aefe34d2
yourball <y.kharkov@gmail.com>
2023-11-24 12:38:14 -0500

    up

--- web_polygons_plotly.py
+++ web_polygons_plotly.py
@@ -86,7 +86,7 @@ def plot_orbits(k_list, xi_list, d, Tmax=1000, map_type="Simple", L=1):
       )
     return fig_map
 
-st.title('Integrable symplectic mappings of the plane')
+st.title('Integrable symplectic mappings of the plane with polygon invariants')
 
 with st.sidebar:
     st.subheader('Map parameters')
