cbdae2f62b1e4a93119d10899d3be80264608995
yourball <y.kharkov@gmail.com>
2023-11-24 00:14:03 -0500

    add map app

---
+++ web_polygons_plotly.py
@@ -0,0 +1,131 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import plotly.express as px
+import altair as alt
+import time
+import matplotlib.pyplot as plt
+
+def force(x, k_list, xi_list, d):
+    # precompute parameters of the force function
+    ai_list, bi_list = [], []
+    for indx in range(len(k_list)):
+        bi = 0
+        for i in range(1, indx):
+            bi += k_list[i] * (xi_list[i] - xi_list[i - 1])
+        if indx > 1:
+            ai = xi_list[indx - 1]
+        else:
+            ai = 0
+        ai_list.append(ai)
+        bi_list.append(bi)
+    ai_list, bi_list = np.array(ai_list), np.array(bi_list)
+
+    intervals = xi_list < x
+    indx = np.count_nonzero(intervals)
+    ki = k_list[indx]
+    ai, bi = ai_list[indx], bi_list[indx]
+    f = ki * (x - ai) + bi + d
+    return f
+
+def periodic_step(x, width=1):
+    return x // width
+
+def force_periodic(x, k_list, xi_list, d):
+    l = xi_list[-1]
+    delta_f = force(l, k_list, xi_list, d) - force(0, k_list, xi_list, d)
+    return delta_f*periodic_step(x, width=l) + force(x % l, k_list, xi_list, d)
+
+def orbit(
+    x0, y0, k_list, xi_list, d, Tmax, map_type="Simple"
+):
+    x, y = x0, y0
+    traj_list = [[x0, y0]]
+
+    for iter in range(Tmax):
+        if map_type == "Simple":
+            x, y = y, -x + force(y, k_list, xi_list, d)
+        elif map_type == "Periodic":
+            x, y = y, -x + force_periodic(y, k_list, xi_list, d)
+
+        traj_list += [[x, y]]
+    traj_data = np.vstack(traj_list)
+    return traj_data
+
+def plot_orbits(k_list, xi_list, d, Tmax=1000, map_type="Simple"):
+    x0_list = np.linspace(0, 5, 10)
+    max_x0 = max(x0_list)
+
+    for i, x0 in enumerate(x0_list):
+
+        y0 = x0*1.0
+        tmp = orbit(x0, y0, k_list, xi_list, d, Tmax=Tmax)
+        if i == 0:
+            traj_data = tmp
+        else:
+            traj_data = np.vstack([traj_data, tmp])
+
+
+    df = pd.DataFrame({'q': traj_data[:, 0], 'p': traj_data[:, 1],
+                      })
+
+    fig_map = px.scatter(df, x='q', y='p',
+                        #color='c'
+                        )
+    fig_map.update_layout(
+        width=600,
+        height=600,
+      )
+    return fig_map
+
+st.title('Integrable symplectic mappings of the plane')
+
+with st.sidebar:
+    st.subheader('Map parameters')
+
+    map_type = st.radio(
+        "Select map type 👉",
+        key="Simple",
+        options=["Simple", "Periodic", "Thorus"],
+    )
+
+    st.divider()
+    k_list = []
+    num_pieces = st.number_input('Number of piecewise regions',
+                                 min_value=2, max_value=10, value=3, step=1)
+    st.text(r'Slopes of the piesewise function')
+    for p in range(num_pieces):
+        k_list.append(st.slider(f'k{p}', min_value=-3, max_value=3, value=0))
+
+    d = st.slider('Shift parameter, d', min_value=-10, max_value=10, value=0)
+    xi_list = np.arange(len(k_list)-1)
+
+    Tmax = st.number_input("Number of map iterations", value=1000, min_value=1, step=100)
+
+st.subheader('Mapping')
+
+print("Plot orbits")
+fig_map = plot_orbits(k_list, xi_list, d=d, Tmax=Tmax, map_type=map_type)
+st.plotly_chart(fig_map)
+
+st.subheader('Force function')
+# plot force function
+x = np.linspace(min(xi_list)-5, max(xi_list)+5, 1000)
+f = []
+
+if map_type == "Simple":
+    f_func = force
+elif map_type == "Periodic":
+    f_func = force_periodic
+
+print(k_list, xi_list)
+for xi in x:
+    f.append(f_func(xi, k_list, xi_list, d))
+df = pd.DataFrame({'x': x, 'f': f})
+fig_f = px.scatter(df, x='x', y='f')
+fig_f.update_layout(
+    width=600,
+    height=600,
+  )
+st.plotly_chart(fig_f)
